dd076b818a535352d851d0d88a7c32a2d116d552
Javier Lasso <fjavierlasso@icloud.com>
2023-03-27 12:51:35 +0200

    refactorizado, limpio y mejorado agregarMovimiento

--- actions/agregarMovimiento.php
+++ actions/agregarMovimiento.php
@@ -1,17 +1,29 @@
 <?php
 include_once "../classes/BaseDatos.php";
-include_once "../classes/campos/CampoTexto.php";
-include_once "../classes/campos/CampoNumerico.php";
 
 session_start(); // Inicia la sesión para almacenar el mensaje de error
 
 $bd = BaseDatos::getInstance();
 
-$campoDescripcion = new CampoTexto();
-$campoValor = new CampoNumerico();
+function validarString($valor)
+{
+    return !empty($valor) && is_string($valor);
+}
 
-// Comprueba si se ha enviado el formulario
+function validarNumber($valor)
+{
+    return !empty($valor) && is_numeric($valor) && $valor > 0 && $valor<99999999.99; // El máximo establecido en la tabla de la BD
+}
+
+function validarTipo($valor)
+{
+    return !empty($valor) && in_array($valor, ['ingreso', 'gasto']);
+}
+
+// Si se ha enviado el formulario...
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Asigno los valores del formulario a variables
     $tipo = isset($_POST['tipo']) ? $_POST['tipo'] : '';
     $descripcion = isset($_POST['descripcion']) ? $_POST['descripcion'] : '';
     $valor = isset($_POST['valor']) ? (float)$_POST['valor'] : 0;
@@ -19,23 +31,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //Si el valor tiene , lo cambia por . (para que sea válido)
     $valor = str_replace(",", ".", $valor);
 
-    // Validación básica y con las clases
-    if (!empty($tipo) && $campoDescripcion->validarDato($descripcion) && $campoValor->validarDato($valor) && in_array($tipo, ['ingreso', 'gasto'])) {
+    // Validación básica de los campos
+    if (validarTipo($tipo) && validarString($descripcion) && validarNumber($valor)) {
         $bd = BaseDatos::getInstance();
 
         // Si todo es válido, inserto el movimiento
         $resultado = $bd->sentencia("INSERT INTO movimientos (tipo, descripcion, valor) VALUES (?,?,?)", $tipo, $descripcion, $valor);
-        
+
         //Cierro conexión con la BD
         $bd->cerrarBD();
 
         header('Location: ../index.php');
-
     } else {
+        // Si no es válido, guardo el mensaje de error en la sesión y redirijo al index
         $_SESSION['error'] = "Por favor, rellena los campos correctamente";
         header('Location: ../index.php');
     }
-
-    
 }
-?>
--- actions/eliminarMovimiento.php
+++ actions/eliminarMovimiento.php
@@ -3,7 +3,7 @@ include_once "../classes/BaseDatos.php";
 
 if (isset($_POST["id"])) {
   $id = $_POST["id"];
-  var_dump($id);
+  var_dump($id); 
   $bd = BaseDatos::getInstance();
 
   $resultado = $bd->ejecutar("DELETE FROM movimientos WHERE id = ?", $id);
